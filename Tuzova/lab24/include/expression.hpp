dd19994aa8126d2cb0bdb855e33c7107b8da1f72
ktuzova <Ksenia.Tuzova12@yandex.ru>
2023-10-03 21:19:09 +0300

    lab24

--- Tuzova/lab24/include/expression.hpp
+++ Tuzova/lab24/include/expression.hpp
@@ -6,21 +6,21 @@
 #include <sstream>
 #include <unordered_map>
 #include <vector>
-#include <cstdlib> // For std::atoi
+#include <cstdlib> 
 
+#include "splitter.hpp"
 
+class Expression{
+public:
+	// Function to find the greatest common divisor (GCD) of two numbers
+	int findGCD(int a, int b);
+	// Function to find the GCD of a vector of numbers
+	int findGCD(const std::vector<int>& numbers) ;
 
-// Function to split the expression into terms
-std::vector<std::string> splitTerms(const std::string& expression) 
-
-// Function to split a term into factors
-std::vector<std::string> splitFactors(const std::string& term)
-
-// Function to find the greatest common divisor (GCD) of two numbers
-int findGCD(int a, int b)
-// Function to find the GCD of a vector of numbers
-int findGCD(const std::vector<int>& numbers) 
-
+	void processTerm(const std::string& term, std::unordered_map<std::string, int>& commonFactors, std::vector<int>& numbers);
+	void processTerms(const std::vector<std::string>& terms, std::unordered_map<std::string, int>& commonFactors, std::vector<int>& numbers);
+	void outputCommonFactors(const std::unordered_map<std::string, int>& commonFactors);
+};
 #include "../src/expression.cpp"
 
 #endif
--- Tuzova/lab24/src/expression.cpp
+++ Tuzova/lab24/src/expression.cpp
@@ -2,34 +2,12 @@
 #include <sstream>
 #include <unordered_map>
 #include <vector>
-#include <cstdlib> // For std::atoi
+#include <cstdlib> 
 
-#include "../include/expresion.hpp"
-
-// Function to split the expression into terms
-std::vector<std::string> splitTerms(const std::string& expression) {
-    std::vector<std::string> terms;
-    std::stringstream ss(expression);
-    std::string term;
-    while (std::getline(ss, term, '+')) {
-        terms.push_back(term);
-    }
-    return terms;
-}
-
-// Function to split a term into factors
-std::vector<std::string> splitFactors(const std::string& term) {
-    std::vector<std::string> factors;
-    std::stringstream ss(term);
-    std::string factor;
-    while (ss >> factor) {
-        factors.push_back(factor);
-    }
-    return factors;
-}
+#include "../include/expression.hpp"
 
 // Function to find the greatest common divisor (GCD) of two numbers
-int findGCD(int a, int b) {
+int Expression::findGCD(int a, int b) {
     if (b == 0) {
         return a;
     }
@@ -37,15 +15,64 @@ int findGCD(int a, int b) {
 }
 
 // Function to find the GCD of a vector of numbers
-int findGCD(const std::vector<int>& numbers) {
+int Expression::findGCD(const std::vector<int>& numbers) {
     if (numbers.empty()) {
         return 0;
     }
     int gcd = numbers[0];
     for (size_t i = 1; i < numbers.size(); ++i) {
-        gcd = findGCD(gcd, numbers[i]);
+        gcd = Expression::findGCD(gcd, numbers[i]);
     }
     return gcd;
 }
+// Функция для обработки терма
+void Expression::processTerm(const std::string& term, std::unordered_map<std::string, int>& commonFactors, std::vector<int>& numbers) {
+    Splitter s1;
+    std::vector<std::string> factors = s1.splitFactors(term);
+    for (const std::string& factor : factors) {
+        size_t pos = factor.find('*');
+        std::string factorWithoutCoefficient = (pos == std::string::npos) ? factor : factor.substr(pos + 1);
+        if (isdigit(factorWithoutCoefficient[0])) {
+            numbers.push_back(std::atoi(factorWithoutCoefficient.c_str()));
+        } else {
+            commonFactors[factorWithoutCoefficient] = 1;
+        }
+    }
+}
+// Функция для обработки всех термов в выражении
+void Expression::processTerms(const std::vector<std::string>& terms, std::unordered_map<std::string, int>& commonFactors, std::vector<int>& numbers) {
+    Splitter s1;
+    for (size_t i = 0; i < terms.size(); ++i) {
+        if (i == 0) {
+            // Process the first term
+            processTerm(terms[i], commonFactors, numbers);
+        } else {
+            // Process the rest of the terms
+            std::unordered_map<std::string, int> newCommonFactors;
+            std::vector<std::string> factors = s1.splitFactors(terms[i]);
+            for (const std::string& factor : factors) {
+                size_t pos = factor.find('*');
+                std::string factorWithoutCoefficient = (pos == std::string::npos) ? factor : factor.substr(pos + 1);
+                if (isdigit(factorWithoutCoefficient[0])) {
+                    numbers.push_back(std::atoi(factorWithoutCoefficient.c_str()));
+                } else if (commonFactors.find(factorWithoutCoefficient) != commonFactors.end()) {
+                    newCommonFactors[factorWithoutCoefficient] = 1;
+                }
+            }
+            commonFactors = newCommonFactors;
+        }
+    }
+}
 
+// Функция для вывода общих множителей
+void Expression::outputCommonFactors(const std::unordered_map<std::string, int>& commonFactors) {
+    bool firstFactor = true;
+    for (const auto& factor : commonFactors) {
+        if (!firstFactor) {
+            std::cout << " * ";
+        }
+        std::cout << factor.first;
+        firstFactor = false;
+    }
+}
 
